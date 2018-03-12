88d57e9c1cb383bacba5d10b4fd16482397a3a19
jelic98 <jelic.ecloga@gmail.com>
2018-03-12 17:07:10 +0100

    Setting config field

--- app/src/main/java/org/ecloga/dashbug/Dashbug.java
+++ app/src/main/java/org/ecloga/dashbug/Dashbug.java
@@ -2,8 +2,8 @@ package org.ecloga.dashbug;
 
 import android.util.Log;
 import java.lang.reflect.Field;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class Dashbug {
 
@@ -15,8 +15,8 @@ public class Dashbug {
     // todo enable/disable Dasbug based on BuildConfig debug flag
 
     /**
-        @param config configuration class
-        @throws  NullPointerException if configuration class is null
+     * @param config configuration class
+     * @throws  NullPointerException if configuration class is null
      */
     public Dashbug(Class config) {
         if(config == null) {
@@ -27,23 +27,74 @@ public class Dashbug {
     }
 
     /**
-     @return map of field name and field value pairs in configuration class
+     * @return map of field name and field value pairs in configuration class
      */
     public HashMap<String, String> getFields() {
         HashMap<String, String> map = new HashMap<>();
 
-        Field[] fields = config.getDeclaredFields();
+        Field[] fields = this.config.getDeclaredFields();
 
         for(Field f : fields) {
+            boolean access = f.isAccessible();
+
             f.setAccessible(true);
 
             try {
-                map.put(f.getName(), f.get(config).toString());
+                map.put(f.getName(), f.get(this.config).toString());
             }catch(Exception e) {
                 Log.e(TAG, e.getMessage());
             }
+
+            f.setAccessible(access);
         }
 
         return map;
+    }
+
+    /**
+     * @param name field name which value will be set
+     * @param value new field value
+     */
+    public void setField(String name, String value) {
+        // todo accept Object value
+
+        Field f;
+
+        try {
+            f = this.config.getDeclaredField(name);
+        }catch(Exception e) {
+            Log.e(TAG, e.getMessage());
+            return;
+        }
+
+        boolean access = f.isAccessible();
+
+        f.setAccessible(true);
+
+        try {
+            if(f.getType().equals(Boolean.TYPE)) {
+                f.setBoolean(this.config, Boolean.parseBoolean(value));
+            }else if(f.getType().equals(Byte.TYPE)) {
+                f.setByte(this.config, Byte.parseByte(value));
+            }else if(f.getType().equals(Character.TYPE)) {
+                f.setChar(this.config, value.charAt(0));
+            }else if(f.getType().equals(Short.TYPE)) {
+                f.setShort(this.config, Short.parseShort(value));
+            }else if(f.getType().equals(Integer.TYPE)) {
+                f.setInt(this.config, Integer.parseInt(value));
+            }else if(f.getType().equals(Long.TYPE)) {
+                f.setLong(this.config, Long.parseLong(value));
+            }else if(f.getType().equals(Float.TYPE)) {
+                f.setFloat(this.config, Float.parseFloat(value));
+            }else if(f.getType().equals(Double.TYPE)) {
+                f.setDouble(this.config, Double.parseDouble(value));
+            }else if(f.getType().equals(String.class)) {
+                f.set(this.config, value);
+            }
+        }catch(Exception e) {
+            Log.e(TAG, e.getMessage());
+        }finally {
+            f.setAccessible(access);
+        }
     }
 }
--- app/src/main/java/org/ecloga/dashbug/MainActivity.java
+++ app/src/main/java/org/ecloga/dashbug/MainActivity.java
@@ -3,8 +3,11 @@ package org.ecloga.dashbug;
 import java.util.Map;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -19,19 +22,32 @@ public class MainActivity extends Activity {
         showFields((ViewGroup) findViewById(R.id.mainLayout));
     }
 
-    private void showFields(ViewGroup parent) {
-        Dashbug db = new Dashbug(AppConfig.class);
+    private void showFields(final ViewGroup parent) {
+        parent.removeAllViews();
+
+        final Dashbug db = new Dashbug(AppConfig.class);
 
         Map<String, String> fields = db.getFields();
 
-        for(String name : fields.keySet()) {
+        for(final String name : fields.keySet()) {
             View vField = getLayoutInflater().inflate(R.layout.field_view, null);
 
             TextView tvName = vField.findViewById(R.id.tvName);
             tvName.setText(name);
 
-            TextView tvValue = vField.findViewById(R.id.tvValue);
-            tvValue.setText(fields.get(name));
+            final EditText etValue = vField.findViewById(R.id.etValue);
+            etValue.setText(fields.get(name));
+            etValue.setHint(name);
+
+            Button btnSet = vField.findViewById(R.id.btnSet);
+            btnSet.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    db.setField(name, etValue.getText().toString());
+
+                    showFields(parent);
+                }
+            });
 
             parent.addView(vField);
         }
